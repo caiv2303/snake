1bc1ee40b5840c9e75c6ffad94ac8bb94c72f766
caiv2303 <160290076+caiv2303@users.noreply.github.com>
2024-05-10 19:08:50 +0000

    Version 2

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <thread>
+#include <string>
+#include <fstream>
+#include <Dibujo.hpp>
+
+using namespace std;
+using namespace ftxui;
+
+int main (int argc, char const *argv[])
+{
+    Dibujo moto("./assets/images/moto.txt");
+    Dibujo bloque("./assets/images/bloque.txt");
+
+    int fotograma=0;
+    while(true)
+    {
+        this_thread::sleep_for(0.1s);
+        fotograma++;
+        Element personaje = spinner (21,fotograma)| bold | color(Color::Purple) | bgcolor(Color::White);
+        Element dibujo = hbox({personaje, moto.GetElement(), bloque.GetElement()});
+        Screen pantalla= Screen::Create(Dimension::Full());
+        Render(pantalla,dibujo);
+        pantalla.Print();
+        cout<<pantalla.ResetPosition();
+    }
+    
+    return EXIT_SUCCESS;
+}
